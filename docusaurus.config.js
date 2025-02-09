526ccfe3eb77a2432660abf3800935bad7cc37c5
Instafomo <developers@instafomo.com>
2025-02-09 23:01:19 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   favicon: "img/favicon.ico",
   staticDirectories: ["static"], // Ensure Docusaurus knows about static files
   // Set the production url of your site here
-  url: "https://instafomo.com",
+  url: "https://docs.instafomo.com/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
